f39d476253e4f85f5401987e8a69b696e6efa137
martinasartori97 <martinasartori9@gmail.com>
2024-11-25 12:04:17 +0100

    edit js

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import Books from "./pages/Books"
 import About from "./pages/About"
 import Contacts from "./pages/Contacts"
+import DefaultLayout from "./pages/DefaultLayout"
 
 function App() {
 
@@ -12,10 +13,11 @@ function App() {
 
 
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/books" element={<Books />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contacts" element={<Contacts />} />
+          <Route element={<DefaultLayout />} >
+            <Route path="/" element={<Home />} />
+            <Route path="/books" element={<Books />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
 
         </Routes>
       </BrowserRouter>
